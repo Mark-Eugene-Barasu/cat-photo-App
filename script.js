5a513dd8fb85baefbb0814fec885495e64f8cda5
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-15 06:17:20 +0200

    [200~

---
+++ script.js
@@ -0,0 +1,4 @@
+{
+//     let holder = document.getElementById('firstLink');
+//     holder.onclick, alert(`Normally this link would bring you to another website! It works. This is a link to: https://freecatphotoapp.com/`);
+}

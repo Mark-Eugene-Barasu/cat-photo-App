bdf218c46624f4618c804758b114c016a2a5a408
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-15 06:22:20 +0200

    This is a #freeCodeCamp beginner to tutorial practice on a Cat Photo App.

--- script.js
+++ script.js
@@ -2,3 +2,4 @@
 //     let holder = document.getElementById('firstLink');
 //     holder.onclick, alert(`Normally this link would bring you to another website! It works. This is a link to: https://freecatphotoapp.com/`);
 }
+// I placed the curly brackets to make the code run not as a windows scoped code
